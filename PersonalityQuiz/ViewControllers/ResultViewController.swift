a3f342ee6f81ffc8985564432fd06beb2ddf8688
Artem Shalin <a.shalin@rtinform.ru>
2022-01-30 16:05:57 +0300

    do resultVC

--- PersonalityQuiz/ViewControllers/ResultViewController.swift
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -8,14 +8,54 @@
 import UIKit
 
 class ResultViewController: UIViewController {
+  
+    // MARK: - IB Outlets
+    @IBOutlet var animalTypeUILabel: UILabel!
+    @IBOutlet var animalDefinitionUILabel: UILabel!
     
-    // 1. Передать сюда массив с ответами
-    // 2. Определить наиболее часто встречающийся тип животного
-    // 3. Отобразить результат в соответствии с этим животным
-    // 4. Избавиться от кнопки возврата на предыдущий экран
+    // MARK: - Public Properties
+    var answersChosen: [Answer]!
 
+    // MARK: - Private Properties
+    private var animalResult: Animal!
+    
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        navigationItem.hidesBackButton = true
+        
+        countAnimals(answers: answersChosen)
+        updateTextInUILabels()
     }
 }
+
+// MARK: - Private Methods
+extension ResultViewController {
+    private func countAnimals(answers: [Answer]) {
+        
+        var animalCounter = [Animal: Int]()
+        
+        for answer in answers {
+            
+            if let animalCount = animalCounter[answer.animal] {
+                animalCounter.updateValue(animalCount + 1, forKey: answer.animal)
+            } else {
+                animalCounter[answer.animal] = 1
+            }
+                
+        }
+        
+        let animalCounterSorted = animalCounter.sorted { $0.value > $1.value }
+        guard let animal = animalCounterSorted.first?.key else { return }
+        
+        animalResult = animal
+    }
+
+    private func updateTextInUILabels() {
+        animalTypeUILabel.text = "Вы - \(animalResult.rawValue)"
+        animalDefinitionUILabel.text = animalResult.definition
+    }
+}
+
+
